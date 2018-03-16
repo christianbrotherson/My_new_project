b5a9d474979d4be72d37ee725f4f225e1deab23a
Christian Brotherson <christian.brotherson@gmail.com>
2018-03-16 11:25:35 -0600

    Some awesome features and changes that I made

---
+++ myfile.py
@@ -0,0 +1,5 @@
+def cool_method():
+    print("This is an awesome method")
+
+def awful_method():
+    print("This is a really bad method that doesn't actually do anyhting")
---
+++ myfile2.py
@@ -0,0 +1,2 @@
+def celeb_quote_of_the_day():
+    print("Wow ~Owen Wilson")
